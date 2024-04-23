2711246c2c55327d4921577f8bacfdf5eb0fdd89
Snerdels <103778173+Snerdels@users.noreply.github.com>
2024-04-23 11:43:50 -0600

    Added Lunar Brace

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/LunarBraceSkill/LunarBraceSkill.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/LunarBraceSkill/LunarBraceSkill.s
@@ -1,39 +1,36 @@
 .thumb
 .equ LunarBraceID, SkillTester+4
 
-push {r4-r7,lr}
+push {r4-r7, lr}
 mov r4, r0 @attacker
 mov r5, r1 @defender
 
-@check for Lunar Brace
+@check for the defender having Lunar Brace
 ldr r0, SkillTester
 mov lr, r0
-mov r0, r4 @attacker data
+mov r0, r5 @defender data
 ldr r1, LunarBraceID
 .short 0xf800
 cmp r0, #0
 beq End
 
+@skip if unit isn't the defender
+mov 	r0, r5
+ldr     r2,=0x203a4ec @defender
+cmp     r0,r2
+bne     End
 
-@and set defender (def or res)=(def or res)*.75
+@and set attacker (def or res)=(def or res)*.75
 mov r1, #0x5C
-ldrh r0, [ r5, r1 ] @Load def/res
+ldrh r0, [ r4, r1 ] @Load def/res
 mov r2, #0x3 
 mul r0,r2 @Multiply def/res by 3
 lsr r0,r0,#0x2 @Divide def/res by 4
-strh r0, [ r5, r1 ]
+strh r0, [ r4, r1 ]
 
-
-cmp r0, #0x7f @damage cap of 127
-ble NotDmgCap
-mov r0, #0x7f
-NotDmgCap:
-strh r0, [r7, #4] @final damage
 
 End:
-pop {r4-r7}
-pop {r15}
-
+pop {r4-r7, r15}
 .align
 .ltorg
 SkillTester:
